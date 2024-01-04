9bdff6ba672c622dfe9d1c039d37deeeb97fa312
READYonHub <104925582+READYonHub@users.noreply.github.com>
2024-01-04 23:33:53 +0100

    Add files via upload

---
+++ animatedCarousel/AnimatedSVG 3/slides.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    id: 1,
+    title: "title1",
+    description: "description1",
+    image: require("./assets/images/kep1.png"),
+  },
+  {
+    id: 2,
+    title: "title2",
+    description: "description2",
+    image: require("./assets/images/kep2.png"),
+  },
+  {
+    id: 3,
+    title: "title3",
+    description: "description3",
+    image: require("./assets/images/kep3.png"),
+  },
+  {
+    id: 4,
+    title: "title4",
+    description: "description4",
+    image: require("./assets/images/kep4.png"),
+  },
+];
